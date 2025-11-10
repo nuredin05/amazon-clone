b72efb875e0b4b3054dbc982f86136bc19277499
nuredin05 <mada4everlove@gmail.com>
2025-11-10 13:52:24 +0300

    after stripe

--- functions/index.js
+++ functions/index.js
@@ -6,7 +6,7 @@ const express=require("express");
 const cors=require("cors");
 const dotenv=require("dotenv");
 dotenv.config();
-const stripe=require("stripe")(process.env.STRIPE_KEY);
+const stripe=require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app=express();
 app.use(cors({origin:true}))
