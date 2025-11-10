12d9b64bb2ecc73c2e1a00efba2a813e48502b43
nuredin05 <mada4everlove@gmail.com>
2025-11-10 15:10:10 +0300

    Fix case-sensitive import for Netlify

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Landing from "./Pages/Landing/Landing";
+import Landing from './Pages/Landing/Landing'
 import Payment from "./Pages/Payment/Payment";
 import Orders from "./Pages/Orders/Orders";
 import Cart from "./Pages/Cart/Cart";
